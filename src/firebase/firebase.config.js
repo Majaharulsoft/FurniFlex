6e244e2b15f1255fc345979fa63e8a8c2021262c
Majaharulsoft <majaharulr1@gmail.com>
2024-09-07 22:18:16 +0600

    update

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -3,23 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBBUceICNwHsktpzxgqvwh_fAHZ9mgnns0",
-  authDomain: "majaharul-furniflex.firebaseapp.com",
-  projectId: "majaharul-furniflex",
-  storageBucket: "majaharul-furniflex.appspot.com",
-  messagingSenderId: "182906198502",
-  appId:  "1:182906198502:web:ce515ef3b67f3b9ec81e27",
-  
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
-
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_apiKey,
-//   authDomain: import.meta.env.VITE_authDomain,
-//   projectId: import.meta.env.VITE_projectId,
-//   storageBucket: import.meta.env.VITE_storageBucket,
-//   messagingSenderId: import.meta.env.VITE_messagingSenderId,
-//   appId: import.meta.env.VITE_appId,
-// };
 
 
 
