bd504c7a0ab0160889383691f93c45747778c4dc
Majaharulsoft <majaharulr1@gmail.com>
2024-09-07 18:32:34 +0600

    solve firebase issue

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,16 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBBUceICNwHsktpzxgqvwh_fAHZ9mgnns0",
-//   authDomain: "majaharul-furniflex.firebaseapp.com",
-//   projectId: "majaharul-furniflex",
-//   storageBucket: "majaharul-furniflex.appspot.com",
-//   messagingSenderId: "182906198502",
-//   appId:  "1:182906198502:web:ce515ef3b67f3b9ec81e27",
-//   // measurementId: import.meta.env.VITE_measurementId,
-// };
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_apiKey,
   authDomain: import.meta.env.VITE_authDomain,
@@ -20,8 +10,6 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_messagingSenderId,
   appId: import.meta.env.VITE_appId,
 };
-
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
